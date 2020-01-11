9432b15a6da551c707d2f0fa874e9e3016afc059
Ayana Reddick <ayanared@gmail.com>
2020-01-11 00:07:16 -0500

    Add helloworld program

---
+++ helloworld/helloworld.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+// to run: $ go run helloworld.go
+// to build (to create a binary): $ go build helloworld.go
+// to run the binary: ./helloworld
