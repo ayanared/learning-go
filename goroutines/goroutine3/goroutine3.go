e2157cb5a3334ef9dd23e02538903f0e4c9e91f0
Ayana Reddick <ayanared@gmail.com>
2020-01-17 17:51:05 -0500

    Add wait groups

---
+++ goroutines/goroutine3/goroutine3.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func count(thing string) {
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
+
+func main() {
+	//create a wait group - which basically holds the number of go routines you want your
+	//program to wait for
+	var wg sync.WaitGroup
+	//The default number is 0, so we need to add a wait group
+	wg.Add(1)
+
+	//create an anonymous function in a goroutine
+	go func() {
+		count("sheep")
+		//decrement the waitgroup counter
+		wg.Done()
+	}()
+
+	//tell the main function to wait till all the wait groups are finished
+
+	wg.Wait()
+}
