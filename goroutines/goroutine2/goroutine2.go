54f2ffb58f5d7e72108f88703820d1415c03f888
Ayana Reddick <ayanared@gmail.com>
2020-01-16 21:08:45 -0500

    Add another goroutine and reorganize directories

---
+++ goroutines/goroutine2/goroutine2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func count(thing string) {
+	for i := 1; true; i++ {
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
+
+func main() {
+	go count("deer")
+	count("sheep")
+	count("fish") //this will never run because sheep will never stop running... The solution is to put
+	//in a goroutine (before the sheep)
+
+}
